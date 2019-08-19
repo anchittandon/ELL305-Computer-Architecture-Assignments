2ead2ffe770442e8e9686b432a6197e78d81b091
anchittandon <mt1170772@iitd.ac.in>
2019-08-19 21:02:10 +0530

    Add SimpleRisc code for mergesort

--- emulator/mergesort.asm
+++ emulator/mergesort.asm
@@ -1,11 +1,118 @@
 .mergesort:
 		@ ADD YOUR CODE HERE
+	cmp r4,r2
+	bgt .if
+	b .break
+	.if:
+		sub r5,r4,r2
+		lsr r5,r5,1
+		add r5,r5,r2
+		lsr r5,r5,2
+		lsl r5,r5,2
 
+		sub sp,sp,16
+
+		st r2,[sp]
+		st r5, 4[sp]
+		st r4, 8[sp]
+		st ra, 12[sp]
+
+		mov r4,r5
+
+		call .mergesort
+
+		ld r2,[sp]
+		ld r5,4[sp]
+		ld r4,8[sp]
+		ld ra,12[sp]
+		
+		add r2,r5,4 
+
+		call .mergesort
+		
+		ld r2,[sp]
+		ld r5,4[sp]
+		ld r4,8[sp]
+		ld ra,12[sp]
+		
+		add sp,sp,16
+
+		sub sp,sp,4
+		st ra,[sp]
+	
+		call .merge
+
+		ld ra,[sp]
+		add sp,sp,4
+
+	.break:
+		ret
 	
 .merge: 	
 		@ ADD YOUR CODE HERE
+	mov r8,r2
+	.loop1:
+		cmp r8,r5
+		bgt .break1
+		ld r9,[r8]
+		st r9,1000[r8]
+		add r8,r8,4
+		b .loop1
+	.break1:
+
+	add r8,r5,4
 
 
+	.loop2:
+		cmp r8,r4
+		bgt .break2
+		ld r9,[r8]
+		st r9,2000[r8]
+		add r8,r8,4
+		b .loop2
+	.break2: 
+
+	mov r8,r2
+	add r9,r5,4
+	mov r10,r2
+	.while:
+		cmp r8,r5
+		bgt .breakwhile1
+		cmp r9,r4
+		bgt .breakwhile2
+		ld r11,1000[r8]
+		ld r12,2000[r9]
+		cmp r11,r12
+		bgt .if1
+		b .else
+		.if1:
+			st r12,[r10]
+			add r9,r9,4
+			b .afterif
+		.else:
+			st r11,[r10]
+			add r8,r8,4
+			b .afterif
+		.afterif:
+			add r10,r10,4
+			b .while
+	.breakwhile1:
+		cmp r9,r4
+		bgt .afterbreak
+		st r12,[r10]
+		add r9,r9,4
+		add r10,r10,4
+		b .breakwhile1
+	.breakwhile2:
+		cmp r8,r5
+		bgt .afterbreak
+		st r11,[r10]
+		add r8,r8,4
+		add r10,r10,4
+		b .breakwhile2
+	.afterbreak: 
+		ret
+		
 .main:
 
 	@ Loading the values as an array into the registers
